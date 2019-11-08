3bc954d54756feacfb919d3e3bcadc8851ecfeb0
Sanjula Madurapperuma <sanjula99@gmail.com>
2019-11-08 11:05:18 +0530

    Adding initial phase of integration tests

---
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/publisher/AuditAPITestCase.java
@@ -0,0 +1,11 @@
+package org.wso2.am.integration.tests.publisher;
+
+import org.apache.commons.logging.LogFactory;
+import org.wso2.am.integration.test.utils.base.APIMIntegrationBaseTest;
+import org.apache.commons.logging.Log;
+import org.wso2.am.integration.test.utils.clients.APIPublisherRestClient;
+
+public class AuditAPITestCase extends APIMIntegrationBaseTest {
+    private static final Log log = LogFactory.getLog(AuditAPITestCase.class);
+    private APIPublisherRestClient apiPublisher;
+}
