63e8fd26ea95c684bf833cc92216e76cc37ca51e
Sanjula Madurapperuma <sanjula99@gmail.com>
2019-12-02 07:04:41 +0530

    Adding properties for sample API

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/APISecurityAuditTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/APISecurityAuditTestCase.java
@@ -40,6 +40,11 @@ import static org.testng.AssertJUnit.assertNotNull;
 
 public class APISecurityAuditTestCase extends APIManagerLifecycleBaseTest {
 
+    private final String AUDIT_API_NAME = "TestAuditAPI";
+    private final String API_CONTEXT = "audit";
+    private final String API_VERSION_1_0_0 = "1.0.0";
+    private final String ENDPOINT_URL = "https://localhost:9443/am-auditAPI-sample";
+
     private ServerConfigurationManager serverConfigurationManager;
     private String apiId;
 
